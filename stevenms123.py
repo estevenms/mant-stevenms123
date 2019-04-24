646f21258df9670940e6cc59300d07eccf26e6f3
stevenms <stemonte131294@gmail.com>
2019-04-24 15:11:22 -0600

    Update stevenms123.py
    Se agrega modificación a la funcion generatePassword

--- stevenms123.py
+++ stevenms123.py
@@ -4,7 +4,9 @@ import string
 que incluyen minúsculas, mayúsculas, números y caracteres especiales """
 def generatePassword():
     password = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(password) for i in range(4,16))
+    """ Retorna un cantraseña aleatoria de la lista de letras, números
+    y caracteres con un tamaña entre 4 y 16 caracteres """
+    return ''.join(random.choice(password) for i in range(random.randint(4,16)))
 print ("Generando contraseña aleatoria ...")
 print ("Contraseña aleatoria:", generatePassword() )
 
